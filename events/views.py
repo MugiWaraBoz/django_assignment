9608ab9c2a35ecb3658588c9afce292f5ee09608
#Mugiwara <nasibislam2003@gmail.com>
2026-01-31 03:27:38 +0600

    merged-models-from-database-branch

--- events/views.py
+++ events/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from events.models import Participant,Event,Category
-from events.forms import EventModelForm
 
 # Create your views here.
 def dashboard(request):
@@ -10,17 +8,4 @@ def event_details(request):
     return render(request, 'event-details.html')
 
 def event_form(request):
-    category = Category.objects.all()
-    participants = Participant.objects.all()
-    form = EventModelForm()
-
-    if request.method == "POST":
-        form = EventModelForm(request.POST)
-        if form.is_valid():
-            form.save()
-    
-    context = {
-        "form": form,
-    }
-
-    return render(request, "event-form.html", context)
+    return render(request, 'event-form.html')
