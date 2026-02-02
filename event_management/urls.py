8b84c1122d8970c9ffc31776ed31da35a20465e4
MugiWaraBoz <nasibislam2003@gmail.com>
2026-02-02 21:34:59 +0600

    Image-path-fix

--- event_management/urls.py
+++ event_management/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('events.urls')),
 ]
+
+# Serve media files in development
+if settings.DEBUG or True:  # Serve media regardless of DEBUG for development
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
