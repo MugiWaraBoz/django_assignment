76d0cc1af84d69e9d59173e034f9132844eb39dc
MugiWaraBoz <nasibislam2003@gmail.com>
2026-02-02 20:27:53 +0600

    Set up for Render

--- event_management/settings.py
+++ event_management/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-$$ze@jbtixh+e*8^(x!41$_-(x9bjq#iud!nep+7y&)s^kzl@k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com', 'http://127.0.0.1:8000/']
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'events/media/')
 MEDIA_URL = '/media/'
